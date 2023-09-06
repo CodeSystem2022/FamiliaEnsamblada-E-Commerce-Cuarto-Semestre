f6e10138a39c742e70dfeb2fe7e00830ca086b77
gfduck <66056030+gfduck@users.noreply.github.com>
2023-09-06 00:31:59 -0300

    Gerardo varios

--- server/endpoints/save.js
+++ server/endpoints/save.js
@@ -1,19 +1,66 @@
 const { functionMysql } = require("../utils/mysql");
-
+const fs = require("fs");
 exports.save = async (req, res) => {
   try {
+    console.log("req file is, ", req.file);
+    console.log("req body is, ", req.body);
+
     const { brand, model, quantity, accesories } = req.body;
 
     const conn = await functionMysql();
     const query = `INSERT INTO products (brand, model, quantity, accesories) VALUES (?, ?, ?, ?)`;
-    conn.query(query, [brand, model, quantity, accesories], function (error, results, fields) {
-      if (error) return res.status(304).json({message:'error'})
-      res.status(200).json({ message: "ok" });
-      conn.end((err) => {
-        //   console.log("Conexión a DB cerrada");
-      });
-    });
+    conn.query(
+      query,
+      [brand, model, quantity, accesories],
+      async function (error, results, fields) {
+        if (error) return res.status(304).json({ message: "error" });
+        const productId = results.insertId;
+        if (req.file) {
+          const image = req.file;
+          const imageExtension = image.originalname.split(".").pop();
+          const imageName = `${productId}.${imageExtension}`;
+          await fs.promises.rename(image.path, `./images/${imageName}`);
+          const updateQuery = `UPDATE products SET image = ? WHERE id_product = ?`;
+          conn.query(
+            updateQuery,
+            [imageName, productId],
+            function (error, results, fields) {
+              if (error) return res.status(304).json({ message: "error" });
+              res.status(200).json({ message: "ok" });
+              conn.end((err) => {
+                //   console.log("Conexión a DB cerrada");
+              });
+            }
+          );
+        } else {
+          res.status(200).json({ message: "ok" });
+          conn.end((err) => {
+            //   console.log("Conexión a DB cerrada");
+          });
+        }
+      }
+    );
   } catch (error) {
     console.log("error es, ", error);
   }
 };
+
+// const { functionMysql } = require("../utils/mysql");
+
+// exports.save = async (req, res) => {
+//   try {
+//     const { brand, model, quantity, accesories } = req.body;
+
+//     const conn = await functionMysql();
+//     const query = `INSERT INTO products (brand, model, quantity, accesories) VALUES (?, ?, ?, ?)`;
+//     conn.query(query, [brand, model, quantity, accesories], function (error, results, fields) {
+//       if (error) return res.status(304).json({message:'error'})
+//       res.status(200).json({ message: "ok" });
+//       conn.end((err) => {
+//         //   console.log("Conexión a DB cerrada");
+//       });
+//     });
+//   } catch (error) {
+//     console.log("error es, ", error);
+//   }
+// };
--- server/index.js
+++ server/index.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const multer = require("multer");
 const { list } = require("./endpoints/list");
 const { update } = require("./endpoints/update");
 const { erase } = require("./endpoints/delete");
 const { save } = require("./endpoints/save");
+const path = require("path");
 app.use(express.json());
+
+const upload = multer({ dest: "images/" });
 app.use(cors());
 app.get("/api/list", list);
-app.post("/api/admin/save", save);
+app.post("/api/admin/save", upload.single("image"), save);
 app.put("/api/admin/update", update);
 app.delete("/api/admin/delete", erase);
+app.use("/images", express.static(path.join(__dirname, "images"))); // serve the images/ directory as a static directory
+
 // GET, POST , PUT, DELETE
 // https://familia-ensamblada.com/api/lista
 //http://localhost:3400/api/lista
 app.listen(3400, function () {
   console.log("Listening on port 3400!");
 });
+//Para correr el server usar nodemon index.js o node index.js
