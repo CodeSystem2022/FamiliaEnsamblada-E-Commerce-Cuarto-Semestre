57b9412647c98e89df0d38d6d920316f9dd214cd
gfduck <66056030+gfduck@users.noreply.github.com>
2023-09-06 00:40:32 -0300

    panel hecho con Matias Villa

---
+++ client/js/newproduct.js
@@ -0,0 +1,27 @@
+const save = document.getElementById("save");
+save.addEventListener("click", (e) => {
+  e.preventDefault();
+  try {
+    console.log("click");
+    const brand = document.getElementById("brand").value;
+    const model = document.getElementById("model").value;
+    const quantity = document.getElementById("quantity").value;
+    const accesories = document.getElementById("accesories").value;
+    const formData = new FormData();
+    formData.append("brand", brand);
+    formData.append("model", model);
+    formData.append("quantity", quantity);
+    formData.append("accesories", accesories);
+    formData.append("image", document.getElementById("formFile").files[0]);
+    console.log("brand es", brand);
+    fetch("http://localhost:3400/api/admin/save", {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("data es", data))
+      .catch((err) => console.log(err));
+  } catch (error) {
+    console.log("error es", error);
+  }
+});
